d9271ab97baf04cfd5f5a61f30bd62bec09fc10a
mohinisri23 <mohinis7007@gmail.com>
2026-07-17 12:08:51 +0530

    backend URL added

--- server/server.ts
+++ server/server.ts
@@ -22,5 +22,5 @@ app.use("/api", itineraryRoutes);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`🚀 Server running on https://trip-planner-cnig.onrender.com/`);
 });
